c1bf55e4a73e13cea67835e88df08425de3345ab
Zoltan Koppanyi <zoltan.koppanyi@gmail.com>
2016-09-12 14:24:55 -0400

    Wifi Fingerprinting data line and data stream has been added.

---
+++ Gaia.Core/DataStreams/WifiFingerprintingDataLine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.Core.DataStreams
+{
+    public class WifiFingerprintingDataLine : DataLine
+    {
+        [System.ComponentModel.DisplayName("RSSI")]
+        public double RSSI { get; set; }
+
+        [System.ComponentModel.DisplayName("MAC")]
+        public char[] MAC { get; set; }
+
+        public override int LineSize()
+        {
+            return sizeof(long) + (2 * sizeof(double) + (6*sizeof(char)));
+        }
+
+        public override void ReadLine(BinaryReader reader)
+        {
+            this.Index = reader.ReadInt64();
+            this.TimeStamp = reader.ReadDouble();
+            this.MAC = reader.ReadChars(6);
+            this.RSSI = reader.ReadDouble();
+
+        }
+
+        public override void WriteLine(BinaryWriter writer)
+        {
+            writer.Write(this.Index);
+            writer.Write(this.TimeStamp);
+            writer.Write(this.MAC);
+            writer.Write(this.RSSI);
+        }
+    }
+}
---
+++ Gaia.Core/DataStreams/WifiFingerptiningDataStream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaia.Core.DataStreams
+{
+    public class WifiFingerptiningDataStream : DataStream
+    {
+        private WifiFingerptiningDataStream(Project project, string fileId) : base(project, fileId)
+        {
+
+        }
+
+        internal static DataStream Create(Project project, string fileId)
+        {
+            DataStream stream = new WifiFingerptiningDataStream(project, fileId);
+            return stream;
+        }
+
+        protected override string extension
+        {
+            get
+            {
+                return "wif";
+            }
+        }
+
+        public override DataLine CreateDataLine()
+        {
+            return new WifiFingerprintingDataLine();
+        }
+    }
+}
